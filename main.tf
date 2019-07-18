6bda2bec48b6abbeb4b72d244e05a4cf61e97153
Steven Black <genseb7@gmail.com>
2019-07-18 15:26:03 -0400

    added core code

---
+++ main.tf
@@ -0,0 +1,31 @@
+
+variable "resource_records" {
+    description = "Google it"
+    type = list(object({
+        data = list(string)
+        name = string
+        ttl = number
+        type = string
+    }))
+    default =[]
+}
+
+variable "common_zone_id" {
+    description = "Zone ID of hosted zone common to all records"
+}
+
+resource "aws_route53_record" "my_dns_records" {
+    # for_each = var.resource_records
+    count = length(var.resource_records)
+
+    allow_overwrite = false
+
+    name = var.resource_records[count.index].name
+    type = var.resource_records[count.index].type
+    zone_id = var.common_zone_id 
+    records = var.resource_records[count.index].data
+}
+
+output "resource_records_list" {
+  value = var.resource_records
+}
